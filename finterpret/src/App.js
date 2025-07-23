a285b631269c2df9f04b72f89f94aa4f9468e374
neilb122 <137945848+neilb122@users.noreply.github.com>
2025-07-23 20:41:23 +0000

    Neil's Changes to Questionnaire

--- finterpret/src/App.js
+++ finterpret/src/App.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PopupQuestionnaire from './components/PopupQuestionnaire';
+
 import Home from './pages/Home';
 import Goals from './pages/Goals';
 import Advisory from './pages/Advisory';
 import Settings from './pages/Settings';
+
 import './App.css';
 
 function App() {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => setShowPopup(false);
+
   return (
     <Router>
       <div className="App">
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/goals">Goals</Link>
-          <Link to="/advisory">Advisory</Link>
-          <Link to="/settings">Settings</Link>
-        </nav>
+        {showPopup && <PopupQuestionnaire onClose={handleClosePopup} />}
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/goals" element={<Goals />} />
-          <Route path="/advisory" element={<Advisory />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
+        {!showPopup && (
+          <>
+            <nav className="navbar">
+              <Link to="/">Home</Link>
+              <Link to="/goals">Goals</Link>
+              <Link to="/advisory">Advisory</Link>
+              <Link to="/settings">Settings</Link>
+            </nav>
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/goals" element={<Goals />} />
+              <Route path="/advisory" element={<Advisory />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </>
+        )}
       </div>
     </Router>
   );
